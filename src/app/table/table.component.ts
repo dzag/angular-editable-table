b2e763603cfed5ffe29aa744e494c06f973c6309
Long Nguyen <contact@longnvh.com>
2018-12-13 15:33:29 +0700

    rewrite: make data worked with new api

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,5 +1,9 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { TableConfigurations } from './core/table-configurations';
+import { TableData } from './core/data/table-data';
+import { TableDataService } from './core/data/table-data.service';
+import { CellManager } from './core/table-cell/cell-manager.service';
+import { CellService } from './core/table-cell/cell.service';
 
 
 @Component({
@@ -7,15 +11,36 @@ import { TableConfigurations } from './core/table-configurations';
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss'],
+  providers: [
+    CellService,
+    CellManager,
+    {
+      provide: TableDataService,
+      useClass: TableDataService,
+      deps: [CellManager, ChangeDetectorRef]
+    }
+  ]
 })
 export class TableComponent implements OnInit, OnDestroy {
 
   @Input() configurations: TableConfigurations;
+  @Input() data: any[];
 
-  constructor (private detectorRef: ChangeDetectorRef) {}
+  public readonly words = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+    'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+  public tableData: TableData;
+
+  constructor (private detectorRef: ChangeDetectorRef,
+               private dataService: TableDataService,
+               private cellManager: CellManager,
+  ) {}
 
   ngOnInit () {
     this.patchConfigs();
+
+    this.tableData = new TableData(this.configurations.states.columns, this.data);
+    this.dataService.tableData = this.tableData;
   }
 
   ngOnDestroy (): void {}
