20680276577313e3152b4d59f9d5cbac663c8d0b
Long Nguyen <contact@longnvh.com>
2018-12-17 06:41:14 +0700

    could group data now!!

--- src/app/table/core/data/table-data.ts
+++ src/app/table/core/data/table-data.ts
@@ -1,4 +1,4 @@
-import { cloneDeep, groupBy, flatMap, isNil, get, mapValues, set, repeat } from 'lodash';
+import { cloneDeep, groupBy, flatMap, isNil, get, mapValues, set, repeat, forEachRight } from 'lodash';
 import * as deepMerge from 'deepmerge';
 import { TableConfigurations } from '../table-configurations';
 
@@ -37,6 +37,7 @@ export class TableData {
     this.initialData = cloneDeep(initialData);
     this.internalData = cloneDeep(initialData);
     this.buildRows(this.internalData, this.descriptors, this.configs.states.rowGroups);
+    this.buildGroupedRows(this.internalData, this.descriptors, this.configs.states.rowGroups);
   }
 
   getCell (row, col, group?) {
@@ -60,7 +61,7 @@ export class TableData {
   }
 
   setCellValue (row, col, group, value: any) {
-    this.setCell(row, col, group, { value });
+    this.setCell(row, col, group, {value});
   }
 
   private patchInitialData (row, col, group, newValue) {
@@ -75,6 +76,111 @@ export class TableData {
     return this.descriptors[column]['prop'];
   }
 
+  private buildGroupedRows<T> (data: Object[], descriptors, rowGroups?: any[]) {
+    if (!data) {
+      return;
+    }
+
+    console.time('abc');
+    const groupedRows = [];
+    rowGroups.forEach((group, groupIndex) => {
+      const criteria = group.groupBy;
+      if (groupIndex === 0) {
+        const dataMap = doGroupFromCriteria(data, criteria);
+        groupedRows.push({
+          group,
+          dataMap
+        });
+      } else {
+        const parent = groupedRows[groupIndex - 1].dataMap;
+        const datas = [];
+        Object.entries(parent).forEach(([key, groupedParent]: [any, any]) => {
+          const dataMap = doGroupFromCriteria(groupedParent, criteria, key + '.');
+          datas.push(dataMap);
+        });
+        const final = {};
+        datas.forEach(d => Object.assign(final, d));
+        groupedRows.push({
+          group,
+          dataMap: final
+        });
+      }
+    });
+
+    let res = [];
+    let prevMap = {};
+    forEachRight(groupedRows, ({ dataMap, group }, index) => {
+      if (index === groupedRows.length - 1) {
+        Object.entries(dataMap).forEach(([k, v]) => {
+          const parentKey = getParentKey(k);
+          const cachedArray = getCachedArray(prevMap, parentKey);
+          cachedArray.push({
+            name: group.name(v[0]),
+            data: v,
+          });
+        });
+      } else if (index > 0) {
+        const copiedPrevMap = {...prevMap};
+        console.log('copiedPrevMap', copiedPrevMap);
+        prevMap = {};
+        const isDirectParent = index === groupedRows.length - 2;
+        Object.entries(copiedPrevMap).forEach(([k, v]) => {
+          const parentKey = getParentKey(k);
+          const cacheArray = getCachedArray(prevMap, parentKey);
+          if (isDirectParent) {
+            cacheArray.push({
+              name: group.name(v[0].data[0]),
+              subGroups: v
+            });
+          } else {
+            // TODO: do this last!!
+          }
+        });
+      } else {
+        const subGroups = repeat('.subGroups[0]', groupedRows.length - 2);
+        Object.entries(prevMap).forEach(([k, v]) => {
+          const _data = get(v, '[0]' + subGroups + '.data[0]');
+          res.push({
+            name: group.name(_data),
+            subGroups: v,
+          });
+        });
+      }
+    });
+    console.timeEnd('abc');
+
+    console.log('groupedRows', groupedRows);
+    console.log('prevMap', prevMap);
+    console.log('res', res);
+
+    function getCachedArray (object, key) {
+      let cachedArray = object[key];
+      if (!cachedArray) {
+        object[key] = cachedArray = [];
+      }
+      return cachedArray;
+    }
+
+    function doGroupFromCriteria (_data, _criteria, parentPath?) {
+      const _dataMap = {};
+      _data.forEach((d, dataIndex) => {
+        const value = !parentPath ? d[_criteria] : parentPath + d[_criteria];
+        let cachedArray = _dataMap[value];
+        if (!cachedArray) {
+          cachedArray = [];
+          _dataMap[value] = cachedArray;
+        }
+        cachedArray.push(d);
+      });
+      return _dataMap;
+    }
+
+    function getParentKey (childKey: string) {
+      const lastDot = childKey.lastIndexOf('.');
+      return lastDot ? childKey.substr(0, lastDot) : childKey;
+    }
+  }
+
   private buildRows<T> (data: Object[], descriptors, rowGroups?: any[]) {
     if (!data) {
       return;
@@ -85,7 +191,7 @@ export class TableData {
     if (isSimple) {
       this.data = data.map(item => {
         const row = [];
-        descriptors.forEach(({ prop, link, transformer }) => {
+        descriptors.forEach(({prop, link, transformer}) => {
           const result: any = {};
           result.value = transformer
             ? transformer(item[prop])
@@ -103,50 +209,6 @@ export class TableData {
 
     if (isGroup) {
 
-      const nest = function (seq, keys) {
-        if (!keys.length) {
-          return seq;
-        }
-        const [first, ...rest] = keys;
-        return mapValues(groupBy(seq, first), value => nest(value, rest));
-      };
-
-      const nestedData = nest(data, rowGroups.map(g => g.groupBy));
-      console.log('nestedData', nestedData);
-
-      const path = [];
-      const indexes = [];
-      let depth = 0;
-      const rs = {};
-      const traverse = (obj) => {
-        Object.entries(obj).forEach(([key, value], index) => {
-          path.push(key);
-          indexes.push(index);
-          if (typeof value === 'object' && Array.isArray(value)) {
-            const array = obj[key];
-            const parentPath = path.join('.');
-          } else if (typeof value === 'object' && !Array.isArray(value)) {
-            depth++;
-            const subs = repeat('subGroups.', depth);
-            const subsPath = key + '.' + subs.substring(0, subs.length - 1);
-            const sub = get(rs, subsPath);
-            if (!sub) {
-              set(rs, subsPath, []);
-            } else {
-              // set(rs, subsPath, sub.concat(value));
-            }
-            console.log(subs.substring(0, subs.length - 1));
-            traverse(obj[key]);
-            depth--;
-          }
-          path.pop();
-          indexes.pop();
-        });
-      };
-
-      traverse(nestedData);
-      console.log(rs);
-
       const group = rowGroups[0];
       const grouped = groupBy(data, group.groupBy);
       this.groupData = grouped;
@@ -159,7 +221,7 @@ export class TableData {
           groupName: group.name(value[0]),
           data: value.map(item => {
             const row = [];
-            descriptors.forEach(({ prop, link, transformer }) => {
+            descriptors.forEach(({prop, link, transformer}) => {
               const result: any = {};
               result.value = transformer
                 ? transformer(item[prop])
@@ -200,7 +262,7 @@ function defaultIndex () {
 }
 
 function romanize (num) {
-  const lookup = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 };
+  const lookup = {M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1};
   let roman = '';
   for (const i in lookup) {
     if (Object.prototype.hasOwnProperty.apply(lookup, i)) {
